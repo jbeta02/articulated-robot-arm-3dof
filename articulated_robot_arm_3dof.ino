5d17e32d510ff217b90b571755e64f8322f2001f
Jesus Betancourt <jbeta02@gmail.com>
2023-06-15 13:44:56 -0700

    remove unneeded line

--- articulated_robot_arm_3dof.ino
+++ articulated_robot_arm_3dof.ino
@@ -21,9 +21,6 @@ void setup() {
 
   Kinematics::arrWrap1 jointAngles = kinematics.getAngles(1.92, 0.41, 0.35, 1, 1, 0);
   kinematics.printMatrix(Serial, "joint angles: ", jointAngles);
-
-  double x = 1001;
-  Serial.println(x);
 }
 
 void loop() { ///////////////////////////////////////////////TODO fix camal case for parameter names
