a0d525af13293e41b80d4cdc136a96ef47e911bd
Jesus Betancourt <jbeta02@gmail.com>
2024-02-17 16:52:20 -0800

    changed test positions

--- articulated_robot_arm_3dof.ino
+++ articulated_robot_arm_3dof.ino
@@ -22,43 +22,42 @@ void setup() {
 
   handServo.setBounds(0, 75);
   servo1.setBounds(0, 180);
-  servo2.setBounds(-60, 0);
-  servo3.setBounds(0, 90);
+  servo2.setBounds(-60, 30);
+  servo3.setBounds(-30, 90);
 
+  servo2.shiftOrigin(180); // shift origin to allow negitive angle inputs
   servo3.shiftOrigin(180); // shift origin to allow negitive angle inputs
 
-  pose = kinematics.getPosition(0, 90, -90, 1, 1, 0);
+  pose = kinematics.getPosition(0, 0, 30, 1, 1, 0);
 
   Serial.begin(9600);
   kinematics.printMatrix(Serial, "end effector pose: ", pose);
 
-  jointAngles = kinematics.getAngles(0, 0, 2, 1, 1, 0);
+  jointAngles = kinematics.getAngles(1.87, 0, 0.5, 1, 1, 0);
   kinematics.printMatrix(Serial, "joint angles (degrees): ", jointAngles);
 }
 
 void loop() { ///////////////////////////////////////////////TODO fix camal case for parameter names and make code follow c++ coding conventions 
   // put your main code here, to run repeatedly:
 
+  handServo.move(0);
+  servo1.move(0);
+  servo2.move(0);
+  servo3.move(0);
+  delay(2000);
+
+  handServo.move(70);
+  delay(2000);
+
+  handServo.move(0);
+  delay(2000);
+
+  servo1.move(0);
+  servo2.move(-30);
+  servo3.move(-30);
+  delay(2000);
+  
   // servo1.move(jointAngles.arr[0][0]);
   // servo2.move(jointAngles.arr[1][0]);
   // servo3.move(jointAngles.arr[2][0]);
-  
-  handServo.move(0);
-  // servo1.move(0);
-  // servo2.move(0);
-  // servo3.move(0);
-  delay(2000);
-
-  // servo0.move(30);
-  // servo1.move(30);
-  // delay(2000);
-
-  // servo0.move(90);
-  // servo1.move(90);
-  // delay(2000);
-
-  // servo0.move(90);
-  // servo1.move(90);
-  // delay(2000);
-
 }
